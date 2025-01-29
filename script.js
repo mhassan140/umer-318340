50113d46373838c5fec780e23e9222e32cc8762b
sameer <156177291+MrRameez@users.noreply.github.com>
2025-01-29 14:52:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// let username = prompt('what is your name?')
+
+// console.log("wellcome " + username)
+
+
+// let greet = prompt('what do you want to do')
+
+
+// console.log("wow " + username + greet)
+
+// alert('pizza','pizz','piz','pi','p')
+
+
+
+let numberofchildren = prompt ('how many children do you have in future')
+
+let partnername = prompt ('what is your partner name')
+
+let jobtitle = prompt ('what will you do in your future job')
